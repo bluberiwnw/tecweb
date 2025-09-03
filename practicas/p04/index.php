689fcbea1dcec02b1b8f7a6d4e9240a6edb29a63
bluberiwnw <garzonsotoraquel@gmail.com>
2025-09-03 01:42:09 -0600

    Ejercicio 4 var. PHP

--- practicas/p04/index.php
+++ practicas/p04/index.php
@@ -130,5 +130,26 @@
     print_r($z);
     ?>
 
+    <h2>Ejercicio 4</h2>
+    <p>Lee y muestra los valores de las variables del ejercicio anterior, pero ahora con la ayuda de
+    la matriz <b>$GLOBALS</b> o del modificador <b>global</b> de PHP.</p>
+
+    <?php
+    $a = 'PHP5';
+    $z[] = &$a;
+    $b = '5a version de PHP';
+    $c = $b*10;
+    $a .= $b;
+    $b *= $c;
+    $z[0] = 'MySQL';
+
+    echo '<h4>Respuesta:</h4>';
+    echo "a = " . $GLOBALS['a'] . "<br>";
+    echo "b = " . $GLOBALS['b'] . "<br>";
+    echo "c = " . $GLOBALS['c'] . "<br>";
+    echo "z = ";
+    print_r($GLOBALS['z']);
+    ?>
+
 </body>
 </html>
