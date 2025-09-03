b5ca63ba9c2e630b298944fbfc6e964e687be6a8
bluberiwnw <garzonsotoraquel@gmail.com>
2025-09-02 22:53:24 -0600

    Ejercicios 1 y 2 var. PHP

---
+++ practicas/p04/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Práctica 4</title>
+</head>
+<body>
+    <h2>Ejercicio 1</h2>
+    <p>Determina cuál de las siguientes variables son válidas y explica por qué:</p>
+    <p>$_myvar,  $_7var,  myvar,  $myvar,  $var7,  $_element1, $house*5</p>
+    <?php
+        //AQUI VA MI CÓDIGO PHP
+        $_myvar;
+        $_7var;
+        //myvar;       // Inválida
+        $myvar;
+        $var7;
+        $_element1;
+        //$house*5;     // Invalida
+        
+        echo '<h4>Respuesta:</h4>';   
+    
+        echo '<ul>';
+        echo '<li>$_myvar es válida porque inicia con guión bajo.</li>';
+        echo '<li>$_7var es válida porque inicia con guión bajo.</li>';
+        echo '<li>myvar es inválida porque no tiene el signo de dolar ($).</li>';
+        echo '<li>$myvar es válida porque inicia con una letra.</li>';
+        echo '<li>$var7 es válida porque inicia con una letra.</li>';
+        echo '<li>$_element1 es válida porque inicia con guión bajo.</li>';
+        echo '<li>$house*5 es inválida porque el símbolo * no está permitido.</li>';
+        echo '</ul>';
+    ?>
+
+    <h2>Ejercicio 2</h2>
+    <p>Proporcionar los valores de $a, $b, $c como sigue:</p>
+
+    <?php
+    echo '<p>$a = "ManejadorSQL";</p>';
+    echo '<p>$b = "MySQL";</p>';
+    echo '<p>$c = &$a;</p>';
+
+    echo '<p>a. Ahora muestra el contenido de cada variable</p>';
+
+    
+    $a = 'ManejadorSQL';
+    $b = 'MySQL';
+    $c = &$a;
+
+    echo '<h4>Respuesta:</h4>';
+
+    print_r($a);
+    echo "<br>";
+    print_r($b);
+    echo "<br>";
+    print_r($c);
+    
+
+     echo '<p>b. Agrega al código actual las siguientes asignaciones:</p>';
+     echo '<p>$a = “PHP server”;</p>';
+     echo '<p>$b = &$a;</p>';
+    
+
+    /*$a = 'ManejadorSQL';
+    $b = 'MySQL';*/
+    $c = &$a;
+    $a = 'PHP server';
+    $b = &$a;
+
+    echo '<h4>Respuesta:</h4>';
+
+    print_r($a);
+    echo "<br>";
+    print_r($b);
+    echo "<br>";
+    print_r($c);
+    ?>
+
+</body>
+</html>
