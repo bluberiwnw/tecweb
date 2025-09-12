e9ab87dc0ba5af88bdbaf4fb6246573a25c23661
bluberiwnw <garzonsotoraquel@gmail.com>
2025-09-11 23:34:38 -0600

    PHP de Respuesta al form Parte 2 y Final

--- practicas/p05/concurso.php
+++ practicas/p05/concurso.php
@@ -11,16 +11,37 @@
     $email = $_POST['email'];
     $telefono = $_POST['phone'];
     $razon = $_POST['descripcion'];  
+    $caractrs = $_POST['caractrs'] ?? []; 
+    $color    = $_POST['color'] ?? 'No seleccionado'; 
+    $talla = $_POST['talla'];
 
-    echo "<h2>MUCHAS GRACIAS</h2>";
-    echo "<p>Gracias por entrar al concurso de Tenis Mike \"Chidos mis Tenis\". ";
+    echo "<h1>MUCHAS GRACIAS</h1>";
+    echo "<p>Gracias por entrar al concurso de Tenis Mike® \"Chidos mis Tenis\". ";
     echo "Hemos recibido la siguiente información de tu registro:</p>";
 
+    echo "<h2>Acerca de ti:</h2>";
     echo "<ul>";
     echo "<li><strong>Nombre:</strong> $nombre</li>";
     echo "<li><strong>Email:</strong> $email</li>";
     echo "<li><strong>Teléfono:</strong> $telefono</li>";
     echo "<li><strong>Tu triste historia:</strong> " . nl2br($razon) . "</li>";
+    echo "</ul>";
+
+    echo "<h2>Tu diseño de Tenis (si ganas):</h2>";
+    echo "<ul>";
+    echo "<li><strong>Color:</strong> $color</li>";
+
+    echo "<li><strong>Características:</strong> ";
+    if (!empty($caractrs)) {
+    foreach ($caractrs as $c) {
+        echo $c . " ";
+    }
+    } else {
+    echo "Ninguna seleccionada";
+    }
+    echo "</li>";
+
+    echo "<li><strong>Talla:</strong> $talla</li>";
     echo "</ul>";
 ?>
 </body>
