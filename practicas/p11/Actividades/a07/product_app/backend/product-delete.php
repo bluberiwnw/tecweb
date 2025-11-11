5906e93f0a36d2bf8f459cefb512ebd177e9c131
bluberiwnw <garzonsotoraquel@gmail.com>
2025-11-10 23:49:35 -0600

    P11 Act. 07 POO P3

---
+++ practicas/p11/Actividades/a07/product_app/backend/product-delete.php
@@ -0,0 +1,27 @@
+<?php
+    use TECWEB\MYAPI\Products as Products;
+    require_once __DIR__ . '/myapi/DataBase.php';
+    $conexion = new mysqli('localhost', 'root', 'Raquel1809*', 'marketzone');
+
+    // SE CREA EL ARREGLO QUE SE VA A DEVOLVER EN FORMA DE JSON
+    $data = array(
+        'status'  => 'error',
+        'message' => 'La consulta falló'
+    );
+    // SE VERIFICA HABER RECIBIDO EL ID
+    if( isset($_POST['id']) ) {
+        $id = $_POST['id'];
+        // SE REALIZA LA QUERY DE BÚSQUEDA Y AL MISMO TIEMPO SE VALIDA SI HUBO RESULTADOS
+        $sql = "UPDATE productos SET eliminado=1 WHERE id = {$id}";
+        if ( $conexion->query($sql) ) {
+            $data['status'] =  "success";
+            $data['message'] =  "Producto eliminado";
+		} else {
+            $data['message'] = "ERROR: No se ejecuto $sql. " . mysqli_error($conexion);
+        }
+		$conexion->close();
+    } 
+    
+    // SE HACE LA CONVERSIÓN DE ARRAY A JSON
+    echo json_encode($data, JSON_PRETTY_PRINT);
+?>
